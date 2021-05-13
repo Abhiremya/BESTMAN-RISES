8927c343b4dd3b8f229ddb5876b5a55a96b94d3f
Abhiremya <77828976+Abhiremya@users.noreply.github.com>
2021-05-14 01:55:05 +0530

    Add files via upload

---
+++ Umbrella.js
@@ -0,0 +1,32 @@
+class Umbrella {
+    constructor(x,y){
+        var options = {
+            isStatic: true,
+        }
+        this.image = loadAnimation("Walking Frame/walking_1.png","Walking Frame/walking_2.png",
+        "Walking Frame/walking_3.png","Walking Frame/walking_4.png","Walking Frame/walking_5.png",
+        "Walking Frame/walking_6.png"),"Walking Frame/walking_7.png","Walking Frame/walking_8.png" ;
+        this.umbrella = Bodies.circle(x,y,50,options);
+        this.radius = 50;
+        World.add(world, this.umbrella)
+        //load Image for BestMan
+        this.batmanImg = loadImage("Walking Frame/batman.png");
+
+    if(frameCount >= 200){
+        image(this.batmanImg,pos.x,pos.y+70,200,300)
+    }
+    
+    }
+
+    display(){
+        var pos = this.umbrella.position;
+        imageMode(CENTER);
+        //display the image for BestMan if the frameCount is over 200, otherwise display the boy with umbrella image
+        if(frameCount >= 200){
+            image(this.batmanImg,pos.x,pos.y+70,200,300)
+        }
+        else{
+          animation(this.image,pos.x,pos.y+70,300,300);}
+        
+    }
+}
